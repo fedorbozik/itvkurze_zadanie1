d3e0bd8af7ed82275a5c7ca010bbd5c0de7ebbce
Fedor Božík <bzk.333@gmail.com>
2018-04-20 20:24:00 +0200

    save

---
+++ js/myScript.js
@@ -0,0 +1,14 @@
+/*function myFunction() {
+    var viewportWidth = $(window).width();
+
+    if (viewportWidth < 768){
+      $( "i[class='fa']" ).addClass('fa-xs');
+    }
+}
+
+document.addEventListener("load", myFunction);
+*/
+jQuery("li").hide()
+/*
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+*/
