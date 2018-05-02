5e3c723b1a8cc5a9d0f44c612eeba170e6a558c6
Fedor Božík <bzk.333@gmail.com>
2018-05-02 20:31:51 +0200

    added a video presentation of smartphone presentation project

--- js/main.js
+++ js/main.js
@@ -3,3 +3,35 @@ AOS.init();
 
 /*smooth scroll initialization*/
 var scroll = new SmoothScroll('a[href*="#"]');
+
+
+/* BACKGROUND VIDEO JS SOURCE */
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#bgvid");
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+}
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed
+vid.pause();
+// to capture IE10
+vidFade();
+});
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+  } else {
+    vid.pause();
+  }
+})
